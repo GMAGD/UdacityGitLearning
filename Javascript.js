c8b4f923ebec5d863aa1b196447b14b51b0b5201
GMAGD <mecegguemallel@gmail.com>
2018-01-03 18:30:46 +0100

    adding some files

---
+++ Javascript.js
@@ -0,0 +1,20 @@
+  var x = 12;
+  var y = 20;
+  const rootedAt = 5;
+  var Data = [5, 6, 7, 8, 1];
+
+  for(var i = 1; i <Data.lenght; i++)
+  {
+    x +=1;
+  }
+  console.log("Not Yet");
+
+  if(x = 20) {
+    console.log("Today is BAC");
+  }
+  function HelloJs(states) {
+      console.log(states);
+    return states;
+  }
+
+HelloJs("I Lova BAC")
